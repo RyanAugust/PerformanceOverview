bc08aec0c4fb7dc34bd29bcf304585e6d912c393
RyanAugust <rad19206@yahoo.com>
2023-06-13 15:57:51 -0700

    Okay kinda a real thing now. CSS standard naming is..... something.... great....

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, url_for, send_file, redirect
+import os
+import csv
 from flask_sqlalchemy import SQLAlchemy
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,6 +8,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///internal.db'
+app.config['UPLOAD_FOLDER'] = 'activity_uploads'
 db = SQLAlchemy(app=app)
 db.init_app(app)
 
@@ -22,6 +25,9 @@ def index():
 
     return render_template("index.html")
 
+@app.route("/modeling", methods=["GET"])
+def modeling_load():
+    return render_template('modeling.html')
 
 @app.route("/activities", methods=['GET'])
 def activities():
@@ -44,27 +50,64 @@ def plot():
     return send_file("plot.png", mimetype="image/png")
 
 
-@app.route("/activityinput", methods=["GET","POST"])
-def submit_activity_input():
-    if request.method == 'POST':
-        # Get the form data
-        activity_datetime = request.form["activity_date"]
-        activity_duration = request.form["activity_duration"]
-        activity_load = request.form["activity_load"]
+@app.route("/activityinput", methods=["GET"])
+def activity_input():
+    return render_template('activityinput.html')
 
-        new_activity = activity_store(
-            activity_date=activity_datetime,
-            activity_duration=activity_duration,
-            activity_load=activity_load)
 
-        try:
-            db.session.add(new_activity)
+@app.route("/activityinput/activitycsv", methods=["POST"])
+def submit_activity_input_csv():
+    # Churn through the csv for new activity data
+    # get the uploaded file
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+        # set the file path
+        uploaded_file.save(file_path)
+        # save the file
+        parseCSV(file_path)
+        delete_file(file_path)
+    try:
+        return redirect('/')
+    except Exception as exc:
+        print(f'Exception :: {exc} occured while commiting new data')
+        return 'There was an issue adding your activity'
+    
+def parseCSV(file_path):
+    with open(file_path) as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for i in reader:
+            kwargs = {column: value for column, value in zip(header, i)}
+            new_entry = activity_store(**kwargs)
+            db.session.add(new_entry)
             db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your activity'
-    else:
-        return render_template('activityinput.html')
+
+def delete_file(file_path:os.path):
+    os.remove(file_path)
+    return 0
+
+@app.route("/activityinput/activity", methods=["POST"])
+def submit_activity_input():
+    # Get the form data
+    activity_datetime_str = request.form["activity_date"]
+    activity_duration = int(request.form["activity_duration"])
+    activity_load = int(request.form["activity_load"])
+
+    activity_datetime = datetime.strptime(activity_datetime_str, "%Y-%m-%d")
+
+    new_activity = activity_store(
+        activity_datetime=activity_datetime,
+        activity_duration=activity_duration,
+        activity_load=activity_load)
+
+    try:
+        db.session.add(new_activity)
+        db.session.commit()
+        return redirect('/activityinput')
+    except Exception as exc:
+        print(f'Exception :: {exc} occured while commiting new data')
+        return 'There was an issue adding your activity'
 
 if __name__ == "__main__":
     app.run(debug=True)
